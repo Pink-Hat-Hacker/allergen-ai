dea88ab6954fb473df1ab4ea1178d903c4698f18
Pink-Hat-Hacker <zyv@udel.edu>
2023-11-16 01:11:50 -0500

    learning radix has hella bugs :3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 import FileUpload from "./components/FileUpload";
 import { detectFood } from "./services/logMealService";
 import { getAllergenData } from "./services/firebaseService";
-import { Box, Grid } from '@radix-ui/themes';
+import { Box, Grid, Flex, Container, Heading} from '@radix-ui/themes';
+
+import "./App.css";
 
 const App = () => {
   const [detectedFood, setDetectedFood] = useState("");
@@ -35,48 +37,48 @@ const App = () => {
 
   return (
     <>
-    <div className="aa-title">
-        <h1>allergen.ai</h1>
-    </div>
-    <Grid columns="2" gap='5' width='auto'>
-      <Box height="auto">
-        <div className="aa-file-upload">
-          <FileUpload onFileUpload={handleFileUpload} />
-        </div>
-      </Box>
-      <Box height="auto">
-        <div className="aa-response container">
-          <Box height='25%'>
-            <div className="aa-response food-detection">
-              <h2>
-                detected food: {detectedFood} with {prob}% probability
-              </h2>
-            </div>
-          </Box>
-          <Box height='25%'>
-            <div className="aa-response ingredient-list">
-              <h3>possible ingredients:</h3>
-              <ul>
-                {nutri.map((ingre) => (
-                  <li key={ingre}>{ingre}</li>
-                ))}
-              </ul>
-            </div>
-          </Box>
-          <Box height='25%'>
-            <div className="aa-response allergen-list">
-              <h3>possible allergens:</h3>
-              <ul>
-                {Object.entries(possibleAllergens).map(([key, values]) => (
-                  <li key={key}>
-                    <strong>{key}:</strong> {values.join(", ")}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </Box>
-        </div>
-      </Box>
+    <Heading size="9">allergen.ai</Heading>
+    <Grid columns='2' width='auto' gap='5'>
+      <Flex direction="column">
+        <Box height="auto" className="aa-file-upload">
+            <FileUpload onFileUpload={handleFileUpload} />
+        </Box>
+      </Flex>
+      <Flex direction="row">
+        <Box height="auto">
+          <Container className="aa-response container">
+            <Box height='25%'>
+              <div className="aa-response food-detection">
+                <h2>
+                  detected food: {detectedFood} with {prob}% probability
+                </h2>
+              </div>
+            </Box>
+            <Box height='25%'>
+              <div className="aa-response ingredient-list">
+                <h3>possible ingredients:</h3>
+                <ul>
+                  {nutri.map((ingre) => (
+                    <li key={ingre}>{ingre}</li>
+                  ))}
+                </ul>
+              </div>
+            </Box>
+            <Box height='25%'>
+              <div className="aa-response allergen-list">
+                <h3>possible allergens:</h3>
+                <ul>
+                  {Object.entries(possibleAllergens).map(([key, values]) => (
+                    <li key={key}>
+                      <strong>{key}:</strong> {values.join(", ")}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Box>
+          </Container>
+        </Box>
+      </Flex>
     </Grid>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Theme accentColor="iris" radius="large" appearance="dark">
       <App />
+      <ThemePanel/>
     </Theme>
   </React.StrictMode>
 );
