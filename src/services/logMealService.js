673c663b4ea15f0d3b5ae15a025512f7b6050fe8
Pink-Hat-Hacker <zyv@udel.edu>
2023-11-18 19:59:21 -0500

    compression and conversion fixed with JPEGs too large, checking for non jpeg next

--- src/services/logMealService.js
+++ src/services/logMealService.js
@@ -1,30 +1,42 @@
 // logMealService.js
 import axios from "axios";
 import { configuration } from "../config";
-import imageConversion from "image-conversion";
+import {filetoDataURL, urltoBlob, compressAccurately} from "image-conversion";
 
-const convertAndCompressImage = (file, targetSizeInMB) => {
+
+const convertAndCompressImage = async (file, targetSizeInMB) => {
+  console.log(file);
+  const isJpeg = file.type === 'image/jpeg';
+
   // Check if the file is already a JPEG and within the size limit
-  if (
-    file.type === "image/jpeg" &&
-    file.size / (1024 * 1024) <= targetSizeInMB
-  ) {
+  if (isJpeg && file.size / (1024 * 1024) <= targetSizeInMB) {
+    console.log("It's a JPEG and it's within the size limit.");
     return file;
   }
 
+  // If it's a JPEG but too big, compress it
+  if (isJpeg) {
+    console.log("It's a JPEG but too big. Compressing...");
+    const compressedBlob = await compressAccurately(file,targetSizeInMB*900);
+    console.log(compressedBlob);
+    // const compressedDataUrl = imageConversion.compress(file, { quality: 0.8 });
+    // const compressedBlob = imageConversion.dataUrlToBlob(compressedDataUrl);
+    return new File([compressedBlob], file.name, { type: 'image/jpeg' });
+  }
+
   // Convert to JPEG and compress
-  const dataUrl = imageConversion.toJpeg(file);
-  const blob = imageConversion.dataUrlToBlob(dataUrl);
+  console.log("It's not a JPEG. Converting and compressing...");
 
-  // Compress the image while maintaining quality
-  const compressedBlob = imageConversion.compress(blob, { quality: 0.8 }); // Adjust quality as needed
-
-  return new File([compressedBlob], file.name, { type: "image/jpeg" });
+  const dataUrl = filetoDataURL(file);
+  const blob = urltoBlob(dataUrl);
+  const compressedBlob = compressAccurately(blob, targetSizeInMB*900);
+  return new File([compressedBlob], file.name, { type: 'image/jpeg' });
 };
 
 export const detectFood = async (file) => {
   try {
-    const compressedFile = convertAndCompressImage(file, 1); // Set your target size in MB
+    const compressedFile = await convertAndCompressImage(file, 1); // Set your target size in MB
+
     const formData = new FormData();
     formData.append("image", compressedFile);
 
@@ -39,9 +51,9 @@ export const detectFood = async (file) => {
       }
     );
 
-    const {imageId} = dishResponse.data;
+    const { imageId } = dishResponse.data;
     const recipeNutriParams = {
-      imageId: imageId
+      imageId: imageId,
     };
 
     const nutriResponse = await axios.post(
@@ -58,7 +70,10 @@ export const detectFood = async (file) => {
     const ingredients = recipe.map((item) => item.name);
 
     return {
-      foodName: dishResponse.data.foodFamily[0]["name"] + " " + nutriResponse.data.foodName,
+      foodName:
+        dishResponse.data.foodFamily[0]["name"] +
+        " " +
+        nutriResponse.data.foodName,
       probability: dishResponse.data.foodFamily[0]["prob"],
       nutriInfo: ingredients,
     };
